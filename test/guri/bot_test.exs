c02ea9aa2d34fa5a0fa996b9a332d1523a16d123
Elvio Vicosa <elvio@elviovicosa.com>
2016-01-08 10:43:34 +0100

    Simplify bot tests with Agents

--- test/guri/bot_test.exs
+++ test/guri/bot_test.exs
@@ -1,44 +1,23 @@
 defmodule Guri.BotTest do
   use ExUnit.Case, async: true
-  alias Guri.{Bot, Command}
+  alias Guri.{Bot, Command, Dispatcher}
 
   defmodule Adapter do
-    use GenServer
-    @behaviour Guri.Adapter.Behaviour
-
-    def start_link(bot), do: GenServer.start_link(__MODULE__, [bot], name: __MODULE__)
-    def init([_bot]), do: {:ok, []}
-    def messages_sent, do: GenServer.call(__MODULE__, :messages_sent)
-    def send_message(message), do: GenServer.cast(__MODULE__, {:send_message, message})
-    def handle_call(:messages_sent, _from, state), do: {:reply, state, state}
-    def handle_cast({:send_message, message}, state), do: {:noreply, [message | state]}
-  end
-
-  defmodule Dispatcher do
-    use GenServer
-    @behaviour Guri.Dispatcher.Behaviour
-
-    def start_link(bot), do: GenServer.start_link(__MODULE__, [bot], name: __MODULE__)
-    def init([_bot]), do: {:ok, []}
-    def handled_commands, do: GenServer.call(__MODULE__, :handled_commands)
-    def dispatch(command), do: GenServer.cast(__MODULE__, {:dispatch, command})
-    def handle_call(:handled_commands, _from, state), do: {:reply, state, state}
-    def handle_cast({:dispatch, command}, state), do: {:noreply, [command | state]}
-  end
-
-  setup do
-    {:ok, _} = Adapter.start_link(Bot)
-    {:ok, _} = Dispatcher.start_link(Bot)
-    {:ok, _} = Bot.start_link(Adapter, Dispatcher)
-    :ok
+    def start_link, do: Agent.start_link(fn -> [] end, name: __MODULE__)
+    def send_message(message), do: Agent.update(__MODULE__, fn(state) -> [message | state] end)
+    def sent_messages, do: Agent.get(__MODULE__, fn(state) -> state end)
   end
 
   test "sends message to adapter" do
+    {:ok, _adapter_pid} = Adapter.start_link()
+    {:ok, _bot_pid} = Bot.start_link(Adapter)
     Bot.send_message("message 1")
-    assert Adapter.messages_sent() == ["message 1"]
+    assert Adapter.sent_messages() == ["message 1"]
   end
 
   test "dispatches command" do
+    {:ok, _dispatcher_pid} = Dispatcher.start_link()
+    {:ok, _bot_pid} = Bot.start_link(nil)
     command = %Command{name: "test"}
     Bot.handle_command(command)
     assert Dispatcher.handled_commands() == [command]
