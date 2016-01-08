d413f80c4887dbcae1cb026ee5a0c28d889173a4
Elvio Vicosa <elvio@elviovicosa.com>
2016-01-08 16:56:20 +0100

    Adds typespec to Guri.Dispatcher

--- lib/guri/dispatcher.ex
+++ lib/guri/dispatcher.ex
@@ -10,6 +10,7 @@ defmodule Guri.Dispatcher do
     {:ok, %{handlers: %{}}}
   end
 
+  @spec register_handler(atom, [String.t]) :: :ok
   def register_handler(module, []) do
     :ok
   end
@@ -18,10 +19,12 @@ defmodule Guri.Dispatcher do
     register_handler(module, tail)
   end
 
+  @spec handler_for(String.t) :: atom
   def handler_for(command_name) do
     GenServer.call(__MODULE__, {:handler_for, command_name})
   end
 
+  @spec dispatch(Guri.Command.t) :: :ok
   def dispatch(command) do
     GenServer.call(__MODULE__, {:dispatch, command})
   end
@@ -38,11 +41,14 @@ defmodule Guri.Dispatcher do
     {:reply, :ok, state}
   end
 
+  @spec dispatch_command(Guri.Command.t, atom) :: :ok
   defp dispatch_command(command, nil) do
     Logger.error("No command handler specified for: #{command.name}")
+    :ok
   end
   defp dispatch_command(command, handler) do
     Logger.error("Dispatching command: #{command.name}")
     handler.handle_command(command)
+    :ok
   end
 end
