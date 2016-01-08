ef5287c38943d921531ae37704209f5bebac3656
Elvio Vicosa <elvio@elviovicosa.com>
2016-01-08 10:51:47 +0100

    Removes dispatch responsibility from Guri.Bot

--- lib/guri/bot.ex
+++ lib/guri/bot.ex
@@ -1,6 +1,5 @@
 defmodule Guri.Bot do
   use GenServer
-  alias Guri.Dispatcher
 
   def start_link(adapter) do
     GenServer.start_link(__MODULE__, [adapter], name: __MODULE__)
@@ -15,17 +14,8 @@ defmodule Guri.Bot do
     GenServer.call(__MODULE__, {:send_message, message})
   end
 
-  @spec handle_command(Guri.Command.t) :: :ok
-  def handle_command(command) do
-    GenServer.call(__MODULE__, {:handle_command, command})
-  end
-
   def handle_call({:send_message, message}, _from, state) do
     state.adapter.send_message(message)
-    {:reply, :ok, state}
-  end
-  def handle_call({:handle_command, command}, _from, state) do
-    Dispatcher.dispatch(command)
     {:reply, :ok, state}
   end
 end
--- test/guri/bot_test.exs
+++ test/guri/bot_test.exs
@@ -1,6 +1,5 @@
 defmodule Guri.BotTest do
   use ExUnit.Case, async: true
-  alias Guri.{Bot, Command, Dispatcher}
 
   defmodule Adapter do
     def start_link, do: Agent.start_link(fn -> [] end, name: __MODULE__)
@@ -10,16 +9,8 @@ defmodule Guri.BotTest do
 
   test "sends message to adapter" do
     {:ok, _adapter_pid} = Adapter.start_link()
-    {:ok, _bot_pid} = Bot.start_link(Adapter)
-    Bot.send_message("message 1")
+    {:ok, _bot_pid} = Guri.Bot.start_link(Adapter)
+    Guri.Bot.send_message("message 1")
     assert Adapter.sent_messages() == ["message 1"]
-  end
-
-  test "dispatches command" do
-    {:ok, _dispatcher_pid} = Dispatcher.start_link()
-    {:ok, _bot_pid} = Bot.start_link(nil)
-    command = %Command{name: "test"}
-    Bot.handle_command(command)
-    assert Dispatcher.handled_commands() == [command]
   end
 end
