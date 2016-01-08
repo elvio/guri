3ae7c2a498b916fed9f4d7d0834e17bcd7244331
Elvio Vicosa <elvio@elviovicosa.com>
2016-01-08 13:58:46 +0100

    Guri Supervisor start processess
    * Starts the bot (passing the adapter name)
    * Starts the dispatcher
    * Starts the adapter itself

--- lib/guri.ex
+++ lib/guri.ex
@@ -1,17 +1,17 @@
 defmodule Guri do
   use Application
 
-  @bot_name Guri.Bot
-
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    children = [
-      worker(Guri.Bot, []),
-      worker(Application.get_env(:guri, :adapter), [@bot_name]),
-      worker(Application.get_env(:guri, :dispatcher), [@bot_name]),
-    ]
+    adapter = Application.get_env(:guri, :adapter)
 
+    children = [
+      worker(Guri.Bot, [adapter]),
+      worker(Guri.Dispatcher, []),
+      worker(adapter, [])
+    ]
+    
     opts = [strategy: :one_for_one, name: Guri.Supervisor]
     Supervisor.start_link(children, opts)
   end
